a141905b59bb8a6c54b9683ddd2533d56e137eb4
Галина Слесарева <galaslesareva@gmail.com>
2024-11-10 13:04:08 +0300

    edit Отображение таймера при загрузке страницы

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import timer from './modules/timer'
 import menu from './modules/menu'
 import modal from './modules/modal'
 
-timer('30 november 2024')
+document.addEventListener("DOMContentLoaded", () => {
+    timer('30 november 2024')
+});
 menu()
 modal()
+
