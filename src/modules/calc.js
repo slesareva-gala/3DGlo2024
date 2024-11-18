bb803982f5b9677601044ebb3a94ca5275355451
Галина Слесарева <galaslesareva@gmail.com>
2024-11-18 15:33:24 +0300

    Задание урока 24

--- src/modules/calc.js
+++ src/modules/calc.js
@@ -1,12 +1,33 @@
-const calc = () => {
-    const elCalc = document.querySelector('.calc-block')
+const calc = (prices = [0, 100, 120, 110]) => {
+    const calcBlock = document.querySelector('.calc-block')
+    const calcType = calcBlock.querySelector('.calc-type')
+    const calcSquare = calcBlock.querySelector('.calc-square')
+    const calcCount = calcBlock.querySelector('.calc-count')
+    const calcDay = calcBlock.querySelector('.calc-day')
+    const total = calcBlock.querySelector('#total')
 
-    elCalc.addEventListener('input', e => {
+    const countCalc = () => {
+        const price = prices[calcType.selectedIndex]
+        const calcTypeValue = +calcType.value
+        const calcSquareValue = +calcSquare.value
+        const calcCountValue = +calcCount.value > 1 ? 1 + calcCount.value * 0.1 : 1
+        const calcDayValue = !+calcDay.value ? 1 : +calcDay.value < 5 ? 2 : +calcDay.value < 10 ? 1.5 : 1
+
+        let totalValue = 0
+
+        if (calcTypeValue && calcSquareValue) {
+            totalValue = price * calcTypeValue * calcSquareValue * calcCountValue * calcDayValue
+        }
+        total.textContent = totalValue
+    }
+
+    calcBlock.addEventListener('input', e => {
         const el = e.target
 
         if (el.closest('input[type="text"]')) {
-            el.value = el.value.replace(/\D/g, '')
+            el.value = +el.value.replace(/\D/g, '') || ''
         }
+        countCalc()
     })
 }
 export default calc
